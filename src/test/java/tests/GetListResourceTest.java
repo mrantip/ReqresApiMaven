9a30f49306c95674925a80f3297877c437388747
Sergei Antipov <mrantip90@gmail.com>
2025-05-23 23:54:56 +0300

    HW 3. added allure steps

--- src/test/java/tests/GetListResourceTest.java
+++ src/test/java/tests/GetListResourceTest.java
@@ -1,20 +1,26 @@
 package tests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.qameta.allure.*;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import models.ResourceResponse;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Epic("Работа ресурсами")
+@Feature("Инфа о ресурсах")
 public class GetListResourceTest {
+    final String BASE_URL = "https://reqres.in/api/unknown";
+    final ObjectMapper objectMapper = new ObjectMapper();
 
+    @Story("Получение информации списке ресурсов")
+    @Severity(SeverityLevel.MINOR)
     @Test
     public void testGetListResource() throws Exception {
-        final String BASE_URL = "https://reqres.in/api/unknown";
-        final ObjectMapper objectMapper = new ObjectMapper();
-
+        step("Get Resource List");
         Response response = RestAssured
                 .given()
                 .when()
@@ -24,6 +30,7 @@ public class GetListResourceTest {
                 .statusCode(200)
                 .extract().response();
 
+        step("Десериализация json-ответа");
         ResourceResponse resourceResponse = objectMapper.readValue(response.asString(), ResourceResponse.class);
 
         assertEquals(12, resourceResponse.getTotal(), "Неверное количество страниц");
